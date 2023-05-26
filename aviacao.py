887c3c6717be536e2ae9e7ea84dd03559c6cc7f1
Chico <chicomady@hotmail.com>
2023-05-26 14:53:48 -0400

    teste

---
+++ aviacao.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+dt = pd.read_csv("ocorrencia.csv", sep=";")
+dt_resumido = dt.iloc[: , 3:10]
+dt_resumido.sort_values(by="Classificacao_da_Ocorrencia",inplace=True)
+dt_Classificacao_da_Ocorrencia= dt_resumido["Classificacao_da_Ocorrencia"]=="Acidente"
+print(dt_resumido)
+print(dt_resumido[dt_Classificacao_da_Ocorrencia])
