03be405d254cbcf313c4337c32746e56d2bae128
Chico <chicomady@hotmail.com>
2023-05-26 14:59:57 -0400

    add new print

--- aviacao.py
+++ aviacao.py
@@ -6,4 +6,4 @@ dt_resumido.sort_values(by="Classificacao_da_Ocorrencia",inplace=True)
 dt_Classificacao_da_Ocorrencia= dt_resumido["Classificacao_da_Ocorrencia"]=="Acidente"
 print(dt_resumido)
 print(dt_resumido[dt_Classificacao_da_Ocorrencia])
-print("Hello")
+
